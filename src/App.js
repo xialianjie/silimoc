3230153d669c14216a0e68a7dcb4655008270a94
夏连杰 <xlj18985364576@163.com>
2025-08-12 11:15:29 +0800

    1

--- src/App.js
+++ src/App.js
@@ -678,6 +678,7 @@ const MainContent = () => {
                 className="text-blue-600 mr-2 h-10"
               />
               <span className="text-lg font-semibold text-gray-800">广东矽联科电子科技有限公司</span>
+              <a href="https://beian.miit.gov.cn/" target="_blank">粤ICP备2025449840号-1</a>
             </div>
 
             <div className="text-gray-600 text-sm">
